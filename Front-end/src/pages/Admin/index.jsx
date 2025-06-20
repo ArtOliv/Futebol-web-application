5a91db49e13ed24e5ad0b231823ac22d0b0b38f2
ArtOliv <arthurcvo1@gmail.com>
2025-06-19 21:39:07 -0300

    Footer com o login do adm, início da página do adm e mudanças

---
+++ Front-end/src/pages/Admin/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+function Admin(){
+    return(
+        <>
+            <div>
+                Esta é a página de Admin
+            </div>
+        </>
+    )
+}
+
+export default Admin
